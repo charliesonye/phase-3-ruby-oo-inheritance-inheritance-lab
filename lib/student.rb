6ece8fd1bdbd03cdd4125dd640e8e9891fcceb5a
Charles <c_onyemem@live.com>
2022-09-22 18:37:16 -0700

    Completed lab

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative './user'
 
+class Student < User
+    def initialize()
+        @knowledge = []    
+    end
+
+    def learn(new_info)
+        @knowledge << new_info
+
+    end
+    
+    def knowledge
+        @knowledge
+    end
+   
 end
